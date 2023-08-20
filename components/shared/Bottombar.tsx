31c89f1046a742e7e98e0c2e6c3eaf83c5d01351
Maxim N <mnosov622@gmail.com>
2023-08-20 02:16:17 -0400

    home page init

---
+++ components/shared/Bottombar.tsx
@@ -0,0 +1,5 @@
+function Bottombar() {
+  return <h1 className="bg-white">Bottom bar</h1>;
+}
+
+export default Bottombar;
---
+++ components/shared/RightSidebar.tsx
@@ -0,0 +1,5 @@
+function RightSidebar() {
+  return <h1 className="bg-white">RightSidebar</h1>;
+}
+
+export default RightSidebar;
