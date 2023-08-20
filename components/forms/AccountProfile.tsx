1ad653926880f6ec00981c43bd69e17085dae98e
Maxim N <mnosov622@gmail.com>
2023-08-20 10:52:12 -0400

    onboarding form update

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -15,6 +15,8 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import * as z from "zod";
+import Image from "next/image";
+import { ChangeEvent } from "react";
 
 interface Props {
   user: {
@@ -45,20 +47,47 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     console.log(values);
   }
 
+  function handleImage(e: ChangeEvent, fieldChange: (value: string) => void) {
+    e.preventDefault();
+  }
+
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col justify-start gap-10">
         <FormField
           control={form.control}
-          name="username"
+          name="profile_photo"
           render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
+            <FormItem className="flex items-center gap-4">
+              <FormLabel className="account-form_image-label">
+                {field.value ? (
+                  <Image
+                    src={field.value}
+                    alt="profile photo"
+                    width={96}
+                    height={96}
+                    priority
+                    className="rounded-full object-contain"
+                  />
+                ) : (
+                  <Image
+                    src="/assets/profile.svg"
+                    alt="profile photo"
+                    width={24}
+                    height={24}
+                    className="object-contain"
+                  />
+                )}
+              </FormLabel>
+              <FormControl className="flex-1 text-base-semibold text-gray-200">
+                <Input
+                  type="file"
+                  accept="image/*"
+                  placeholder="Upload a photo"
+                  className="account-form_image-input"
+                  onChange={(e) => handleImage(e, field.onChange)}
+                />
               </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
-              <FormMessage />
             </FormItem>
           )}
         />
