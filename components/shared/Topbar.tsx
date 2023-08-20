f2998fe0b45fe124fd6e383094d3ee8696d7ad67
Maxim N <mnosov622@gmail.com>
2023-08-20 03:00:42 -0400

    right sidebar theme update

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignedIn, SignOutButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from "@clerk/themes";
 
 function TopBar() {
   return (
@@ -21,6 +22,7 @@ function TopBar() {
         </div>
         <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: "py-2 px-4",
             },
