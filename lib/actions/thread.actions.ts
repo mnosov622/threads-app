fad621ed298ebc4283995e32c47778efdb03eb5e
Maxim N <mnosov622@gmail.com>
2023-08-22 01:43:20 -0400

    create thread function update

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -1,4 +1,6 @@
+import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
 interface Params {
@@ -11,5 +13,16 @@ interface Params {
 export async function createThread({ text, author, communityId, path }: Params) {
   connectToDB();
 
-  const createdThread = await Thread.create();
+  const createdThread = await Thread.create({
+    text,
+    author,
+    community: null,
+  });
+
+  //Update user model
+  await User.findByIdAndUpdate(author, {
+    $push: { threads: createdThread._id },
+  });
+
+  revalidatePath(path);
 }
