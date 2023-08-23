57c8fe6f835ac2bc612c7e99d3082ca9c740d36c
Maxim N <mnosov622@gmail.com>
2023-08-23 03:44:40 -0400

    threads tab component added

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,32 +1,67 @@
-import ProfileHeader from "@/components/shared/ProfileHeader";
-import { fetchUser } from "@/lib/actions/user.actions";
+import Image from "next/image";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
+import { profileTabs } from "@/constants";
+
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import { fetchUser } from "@/lib/actions/user.actions";
+
 async function Page({ params }: { params: { id: string } }) {
   const user = await currentUser();
-
   if (!user) return null;
 
-  const userInfo: any = await fetchUser(params.id);
-
-  // If user has not onboarded, redirect to onboarding page
-  if (!userInfo[0]?.onboarded) {
-    redirect("/onboarding");
-  }
+  const userInfo = await fetchUser(params.id);
+  if (!userInfo[0]?.onboarded) redirect("/onboarding");
 
   return (
-    <>
+    <section>
       <ProfileHeader
-        accountId={userInfo[0]._id.toString()}
+        accountId={userInfo[0].id}
         authUserId={user.id}
         name={userInfo[0].name}
         username={userInfo[0].username}
         imgUrl={userInfo[0].image}
         bio={userInfo[0].bio}
       />
-    </>
+
+      <div className="mt-9">
+        <Tabs defaultValue="threads" className="w-full">
+          <TabsList className="tab">
+            {profileTabs.map((tab) => (
+              <TabsTrigger key={tab.label} value={tab.value} className="tab">
+                <Image
+                  src={tab.icon}
+                  alt={tab.label}
+                  width={24}
+                  height={24}
+                  className="object-contain"
+                />
+                <p className="max-sm:hidden">{tab.label}</p>
+
+                {tab.label === "Threads" && (
+                  <p className="ml-1 rounded-sm bg-light-4 px-2 py-1 !text-tiny-medium text-light-2">
+                    {userInfo[0]?.threads?.length}
+                  </p>
+                )}
+              </TabsTrigger>
+            ))}
+          </TabsList>
+          {profileTabs.map((tab) => (
+            <TabsContent
+              key={`content-${tab.label}`}
+              value={tab.value}
+              className="w-full text-light-1"
+            >
+              {/* @ts-ignore */}
+              {/* <ThreadsTab currentUserId={user.id} accountId={userInfo[0].id} accountType="User" /> */}
+            </TabsContent>
+          ))}
+        </Tabs>
+      </div>
+    </section>
   );
 }
-
 export default Page;
