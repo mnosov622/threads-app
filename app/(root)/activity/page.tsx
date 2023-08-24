89347dbe406cf3524c1ff6bcf46f441101740f75
Maxim N <mnosov622@gmail.com>
2023-08-24 09:36:23 -0400

    sign in form position update

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,7 +1,49 @@
-const Page = () => {
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+// import UserCard from "@/components/cards/UserCard";
+// import Searchbar from "@/components/shared/Searchbar";
+// import Pagination from "@/components/shared/Pagination";
+import UserCard from "@/components/cards/UserCard";
+import Link from "next/link";
+import Image from "next/image";
+
+const Page = async () => {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  //   if (!userInfo?[0].onboarded) redirect("/onboarding");
+
+  //get Activity
+
+  const activity = await getActivity(userInfo[0]._id);
+
   return (
     <section>
       <h1 className="head-text mb-10">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
+          <>
+            {activity.map((activity) => {
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt="Profile Picture"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                </article>
+              </Link>;
+            })}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
+        )}
+      </section>
     </section>
   );
 };
