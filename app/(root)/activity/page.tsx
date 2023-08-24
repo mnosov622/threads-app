070b77ec9bc9c69aa305f2bce8596f26ecedaa16
Maxim N <mnosov622@gmail.com>
2023-08-24 09:49:10 -0400

    display activity on activity page

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -26,19 +26,23 @@ const Page = async () => {
       <section className="mt-10 flex flex-col gap-5">
         {activity.length > 0 ? (
           <>
-            {activity.map((activity) => {
+            {activity.map((activity) => (
               <Link key={activity._id} href={`/thread/${activity.parentId}`}>
                 <article className="activity-card">
                   <Image
                     src={activity.author.image}
-                    alt="Profile Picture"
+                    alt="user_logo"
                     width={20}
                     height={20}
                     className="rounded-full object-cover"
                   />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">{activity.author.name}</span> replied to
+                    your thread
+                  </p>
                 </article>
-              </Link>;
-            })}
+              </Link>
+            ))}
           </>
         ) : (
           <p className="!text-base-regular text-light-3">No activity yet</p>
