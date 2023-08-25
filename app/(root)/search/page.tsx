28ba4a7d8b91ba6443d817daf7276a9eac938620
Maxim N <mnosov622@gmail.com>
2023-08-25 08:28:09 -0400

    pagination component added

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,12 +1,11 @@
 import { redirect } from "next/navigation";
 import { currentUser } from "@clerk/nextjs";
 
-// import UserCard from "@/components/cards/UserCard";
-// import Searchbar from "@/components/shared/Searchbar";
-// import Pagination from "@/components/shared/Pagination";
+import UserCard from "@/components/cards/UserCard";
+import Searchbar from "@/components/shared/Searchbar";
+import Pagination from "@/components/shared/Pagination";
 
 import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-import UserCard from "@/components/cards/UserCard";
 
 async function Page({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
   const user = await currentUser();
@@ -26,7 +25,7 @@ async function Page({ searchParams }: { searchParams: { [key: string]: string | 
     <section>
       <h1 className="head-text mb-10">Search</h1>
 
-      {/* <Searchbar routeType="search" /> */}
+      <Searchbar routeType="search" />
 
       <div className="mt-14 flex flex-col gap-9">
         {result.users.length === 0 ? (
@@ -47,11 +46,11 @@ async function Page({ searchParams }: { searchParams: { [key: string]: string | 
         )}
       </div>
 
-      {/* <Pagination
+      <Pagination
         path="search"
         pageNumber={searchParams?.page ? +searchParams.page : 1}
         isNext={result.isNext}
-      /> */}
+      />
     </section>
   );
 }
