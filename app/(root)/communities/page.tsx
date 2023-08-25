70f4cdc96cef96868f18559f16fa4a4c2a5a6435
Maxim N <mnosov622@gmail.com>
2023-08-25 07:14:48 -0400

    community card added

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,59 @@
-const Page = () => {
+import { redirect } from "next/navigation";
+import { currentUser } from "@clerk/nextjs";
+
+// import UserCard from "@/components/cards/UserCard";
+// import Searchbar from "@/components/shared/Searchbar";
+// import Pagination from "@/components/shared/Pagination";
+
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchUser } from "@/lib/actions/user.actions";
+
+async function Page({ searchParams }: { searchParams: { [key: string]: string | undefined } }) {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  // if (!userInfo?[0].onboarded) redirect("/onboarding");
+
+  const result = await fetchCommunities({
+    searchString: searchParams.q,
+    pageNumber: searchParams?.page ? +searchParams.page : 1,
+    pageSize: 25,
+  });
+
   return (
     <section>
       <h1 className="head-text mb-10">Communities</h1>
+
+      {/* <Searchbar routeType="search" /> */}
+
+      <div className="mt-14 flex flex-col gap-9">
+        {result.communities.length === 0 ? (
+          <p className="no-result">No Result</p>
+        ) : (
+          <>
+            {result.communities.map((community) => (
+              <UserCard
+                key={community.id}
+                id={community.id}
+                name={community.name}
+                username={community.username}
+                imgUrl={community.image}
+                personType="User"
+              />
+            ))}
+          </>
+        )}
+      </div>
+
+      {/* <Pagination
+        path="search"
+        pageNumber={searchParams?.page ? +searchParams.page : 1}
+        isNext={result.isNext}
+      /> */}
     </section>
   );
-};
+}
 
 export default Page;
